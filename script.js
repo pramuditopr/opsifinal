7d6e0f06628f37d834c6fe03a095cd22ad578008
prmdt <pramuditoens25@gmail.com>
2025-10-07 17:04:35 +0700

    commit harian

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const webcamElement = document.getElementById('webcam');
 const captureBtn = document.getElementById('captureBtn');
 
 // Teachable Machine Model
-const URL = "https://teachablemachine.withgoogle.com/models/mmvyDFj6z/";
+const URL = "https://teachablemachine.withgoogle.com/models/wv7FAqivr/";
 let model, webcam, labelContainer, maxPredictions;
 
 // Initialize the model
